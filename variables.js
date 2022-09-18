cc4093ab696503e0411f82a42faca786bbb10b53
MayBertholaga <113070598+MayBertholaga@users.noreply.github.com>
2022-09-18 18:06:04 -0300

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,8 @@
+var prenda="remera";
+var talle="M";
+var color= "Amarillo";
+var prenda_y_talle= prenda + " "+ talle + " ";
+prenda= "pantalon";
+talle="42";
+console.log (prenda,talle, color);
+alert(remera_y_talle);
